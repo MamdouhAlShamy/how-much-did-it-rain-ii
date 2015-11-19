4c30e99e2502e3e3b3f8776d2fb656a0e2a14a93
mamdouh.al.shamy <mamdouh.al.shamy@gmail.com>
2015-11-19 03:19:02 +0100

    Correlation between Expected and all other features separely

---
+++ Code/Raw/exp_correlation_ffdf.r
@@ -0,0 +1,38 @@
+library(ff)
+library(ffbase)
+
+load.ffdf(dir="Data/Raw/train_ffdf")
+
+
+x = matrix()
+
+limit = 3:23
+
+for (i in limit){
+	m = cor(x = train[,i], y = train[, 24], use="complete.obs")
+	x <<- rbind(x, m)
+	print(paste0(m, " ", i, " col DONE"))
+}
+
+# print(mcor)
+# colnames(mcor) = colnames(train)
+cor_df = data.frame(featureName = colnames(train)[limit]
+	, correlation = as.vector(x[-1,]) ) 
+# row.names(cor_df) = colnames(train)[limit]
+
+print(cor_df)
+
+library(ggplot2)
+library(gridExtra)
+
+svg("Figures/Raw/Correlation_ffdf.svg")
+ggplot(cor_df
+	, aes(x=featureName, y= correlation, fill=correlation))+
+geom_bar(stat="identity")+
+theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+scale_color_brewer(type = "div", palette = 2)+
+scale_y_continuous(limits=c(-0.25, 0.25))
+
+
+
+dev.off()
